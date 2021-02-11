13b7244a0f5efa2bb162551005768c9ec6f2c96b
Duc Pham <dkypham@gmail.com>
2021-02-10 20:22:40 -0800

    Removed magic numbers from Resources files.

--- economy/Resources.cpp
+++ economy/Resources.cpp
@@ -1,75 +1,81 @@
 #include "Resources.h"
 
 namespace Resources {
-// dResources[0] = Actual mineral amount
-// dResources[1] = Effective mineral amount
-// dResources[2] = Actual gas amount
-// dResources[3] = Effective gas amount
-// dResources[4] = Actual available supply amount
-// dResources[5] = Effective available supply amount
-// dResources[6] = Actual supply used
+  // dResources[0] = Actual mineral amount
+  // dResources[1] = Effective mineral amount
+  // dResources[2] = Actual gas amount
+  // dResources[3] = Effective gas amount
+  // dResources[4] = Actual available supply amount
+  // dResources[5] = Effective available supply amount
+  // dResources[6] = Actual supply used
 
-  void initializeResources( std::vector<int>& dResources ) {
-    dResources[0] = BWAPI::Broodwar->self()->minerals();
-    dResources[1] = BWAPI::Broodwar->self()->minerals();
-    dResources[2] = BWAPI::Broodwar->self()->gas();
-    dResources[3] = BWAPI::Broodwar->self()->gas();
-    dResources[4] = BWAPI::Broodwar->self()->supplyTotal()/2;
-    dResources[5] = BWAPI::Broodwar->self()->supplyTotal()/2;
-    dResources[6] = BWAPI::Broodwar->self()->supplyUsed()/2;
+  void initializeResources(std::vector<int>& dResources) {
+    dResources[kActMinIndex] = BWAPI::Broodwar->self()->minerals();
+    dResources[kEffMinIndex] = BWAPI::Broodwar->self()->minerals();
+    dResources[kActGasIndex] = BWAPI::Broodwar->self()->gas();
+    dResources[kEffGasIndex] = BWAPI::Broodwar->self()->gas();
+    dResources[kActSupIndex] = BWAPI::Broodwar->self()->supplyTotal() / 
+      kSupplyModifier;
+    dResources[kEffSupIndex] = BWAPI::Broodwar->self()->supplyTotal() / 
+      kSupplyModifier;
+    dResources[kSupUseIndex] = BWAPI::Broodwar->self()->supplyUsed() / 
+      kSupplyModifier;
   }
 
-  void updateResources( std::vector<int>& dResources , 
-    std::multimap<BWAPI::UnitType, int>& dArmyMap ) {
-    dResources[0] = BWAPI::Broodwar->self()->minerals();
-    dResources[1] = BWAPI::Broodwar->self()->minerals();
-    dResources[2] = BWAPI::Broodwar->self()->gas();
-    dResources[3] = BWAPI::Broodwar->self()->gas();
-    dResources[4] = BWAPI::Broodwar->self()->supplyTotal()/2;
-    dResources[5] = BWAPI::Broodwar->self()->supplyTotal()/2;
-    dResources[6] = BWAPI::Broodwar->self()->supplyUsed()/2;
+  void updateResources(std::vector<int>& dResources,
+                       std::multimap<BWAPI::UnitType, int>& dArmyMap) {
+    dResources[kActMinIndex] = BWAPI::Broodwar->self()->minerals();
+    dResources[kEffMinIndex] = BWAPI::Broodwar->self()->minerals();
+    dResources[kActGasIndex] = BWAPI::Broodwar->self()->gas();
+    dResources[kEffGasIndex] = BWAPI::Broodwar->self()->gas();
+    dResources[kActSupIndex] = BWAPI::Broodwar->self()->supplyTotal() /
+      kSupplyModifier;
+    dResources[kEffSupIndex] = BWAPI::Broodwar->self()->supplyTotal() /
+      kSupplyModifier;
+    dResources[kSupUseIndex] = BWAPI::Broodwar->self()->supplyUsed() /
+      kSupplyModifier;
   }
 
-  bool isEnoughMinerals( std::vector<int> dResources, int mineralsCost ) {
-    return dResources[1] - mineralsCost >= 0;
+  bool isEnoughMinerals(std::vector<int> dResources, int mineralsCost) {
+    return dResources[kEffMinIndex] - mineralsCost >= 0;
   }
 
-  bool isEnoughGas( std::vector<int> dResources, int gasCost ) {
-    return dResources[3] - gasCost >= 0;
+  bool isEnoughGas(std::vector<int> dResources, int gasCost) {
+    return dResources[kEffGasIndex] - gasCost >= 0;
   }
 
-  bool isEnoughMineralsGas( std::vector<int> dResources, int mineralsCost ,
-    int gasCost ) {
-    return (dResources[1] - mineralsCost >= 0) && 
-      (dResources[3] - gasCost >= 0);
+  bool isEnoughMineralsGas(std::vector<int> dResources, int mineralsCost,
+                           int gasCost) {
+    return (dResources[kEffMinIndex] - mineralsCost >= 0) &&
+      (dResources[kEffGasIndex] - gasCost >= 0);
   }
 
-  void updateEffectiveMinerals( std::vector<int> dResources, 
-    int mineralsCost ) {
+  void updateEffectiveMinerals(std::vector<int> dResources,
+                               int mineralsCost) {
     dResources[1] = dResources[1] - mineralsCost;
   }
 
-  void updateEffectiveGas( std::vector<int> dResources,
-    int gasCost ) {
-    dResources[3] = dResources[3] - gasCost;
+  void updateEffectiveGas(std::vector<int> dResources,
+                          int gasCost) {
+    dResources[kEffGasIndex] = dResources[kEffGasIndex] - gasCost;
   }
 
-  void updateEffectiveMineralsGas( std::vector<int> dResources,
-    int mineralsCost , int gasCost) {
-    dResources[1] = dResources[1] - mineralsCost;
-    dResources[3] = dResources[3] - gasCost;
+  void updateEffectiveMineralsGas(std::vector<int> dResources,
+                                  int mineralsCost, int gasCost) {
+    dResources[kEffMinIndex] = dResources[kEffMinIndex] - mineralsCost;
+    dResources[kEffGasIndex] = dResources[kEffGasIndex] - gasCost;
   }
 
-  void updateEffectiveResources( std::vector<int> dResources, 
-    std::multimap<BWAPI::UnitType, int>& dArmyMap) {
-    for (auto itr = dArmyMap.find( BWAPI::UnitTypes::Terran_SCV ); 
-      itr != dArmyMap.end(); itr++ ) {
+  void updateEffectiveResources(std::vector<int> dResources,
+                                std::multimap<BWAPI::UnitType, int>& dArmyMap) {
+    for (auto itr = dArmyMap.find(BWAPI::UnitTypes::Terran_SCV);
+         itr != dArmyMap.end(); itr++) {
       //BWAPI::Broodwar << "SCV ID: " << itr->second << std::endl;
 
     }
   }
 
-  void updateEffectiveSupply( std::vector<int> dResources ) {
+  void updateEffectiveSupply(std::vector<int> dResources) {
 
   }
 
--- economy/Resources.h
+++ economy/Resources.h
@@ -3,6 +3,15 @@
 
 namespace Resources{
   const int kResourcesSize = 7;
+  const int kSupplyModifier = 2; // divide by 2 to match in game supply
+  const int kActMinIndex = 0;
+  const int kEffMinIndex = 1;
+  const int kActGasIndex = 2;
+  const int kEffGasIndex = 3;
+  const int kActSupIndex = 4;
+  const int kEffSupIndex = 5;
+  const int kSupUseIndex = 6;
+
 
   void initializeResources( std::vector<int>& dResources );
   void updateResources( std::vector<int>& dResources , 
